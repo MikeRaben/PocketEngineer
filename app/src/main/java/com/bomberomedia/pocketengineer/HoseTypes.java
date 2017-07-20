d5dd4f0eb464801f6d51e6ebc24ce174446052c2
Mike Raben <raben.mike@gmail.com>
2017-07-20 10:58:37 -0600

    initial commit

---
+++ app/src/main/java/com/bomberomedia/pocketengineer/HoseTypes.java
@@ -0,0 +1,16 @@
+package com.bomberomedia.pocketengineer;
+
+import java.util.ArrayList;
+
+class HoseTypes {
+    Double diameter;
+    Double coefficient;
+    Boolean active;
+
+    HoseTypes(Double diam, Double coeff, Boolean act){
+        this.diameter = diam;
+        this.coefficient = coeff;
+        this.active = act;
+    }
+
+}
